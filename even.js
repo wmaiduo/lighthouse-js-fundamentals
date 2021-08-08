bf85345ef3a6f41e37cd619da8c3510c16b540ab
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-08 00:38:31 +0000

    added isEven

---
+++ even.js
@@ -0,0 +1,14 @@
+const is Even = function (num) {
+	return num % 2 === 0;
+}
+
+const tenIsEven = isEvenconst isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
