d4470d94c6c284ce8252ea34563ecfe1129a913b
Mai Duo Wu <wmaiduo@gmail.com>
2021-08-08 06:03:50 +0000

    doing loop range exercise

--- exercise.js
+++ exercise.js
@@ -1,3 +1,7 @@
-const calculateRectangleArea = (length, width) => length * width > 0 ? length * width : undefined;
-const calculateTriangleArea = (base, height) => base * height / 2 > 0 ? base * height / 2 : undefined;
-const calculateCircleArea = (radius) => radius > 0 ? Math.PI * radius * radius : undefined;
+function range(start, end, step) {
+	let range = [];
+	for (let i = start; i <= end; i += step){
+		range.push(i);
+	}
+	return range;
+}
